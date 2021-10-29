0ba9c4600bc2f64b9b2432346b5564c778f6acbd
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2021-10-29 15:17:31 +0100

    can win and lose and draw

--- checkers.py
+++ checkers.py
@@ -6,7 +6,16 @@ Checkers code by Dexter Shepherd, aged 20
 
 import pygame
 import copy
+import time
 
+class AI_agent:
+    """
+    AI player which uses a mini max algorithm to find the best option through a game
+
+    @param difficulty that the AI will play at
+    """
+    def __init__(self,difficulty):
+        self.difficulty=difficulty
 class Piece:
     """
     Piece code will store the piece information
@@ -14,12 +23,15 @@ class Piece:
     whether it is crowned
 
     These conditions will specify what moves a piece can do
+
+    @param player that the piece belongs to (int)
     """
     def __init__(self,player):
         self.crowned=False
         self.player=player
         self.pos=[]
         self.toggle=False
+    #setters and getters defined below
     def setPosition(self,arr):
         self.pos=arr
     def getPlayer(self):
@@ -30,12 +42,12 @@ class Piece:
         return self.crowned
     def crown(self):
         self.crowned=True
-    def toggleSelected(self):
+    def toggleSelected(self): #toggle the selected value on and off
         self.toggle= not self.toggle
     def getSelected(self):
         return self.toggle
     def returnVectors(self):
-        #return the possible vectos
+        #return the possible vectors that the piece can take
         vectors=[]
         if self.player==1:
             if self.crowned: #can go both directions
@@ -56,7 +68,7 @@ class GameBoard:
     """
     def __init__(self):
         self.grid=[[None for j in range(8)] for i in range(8)] #populate empty board
-        #populate player pieces
+        #populate player pieces within the set pattern
         for j in range(8):
             if j%2!=0: self.grid[0][j]=Piece(1)
         for j in range(8):
@@ -75,13 +87,13 @@ class GameBoard:
             for j in range(8):
                 if self.grid[i][j]!=None:
                     count[str(self.grid[i][j].getPlayer())]+=1 #count how many of each player exist
-        if count['1']>0 and count['2']==0:
+        if count['1']>0 and count['2']==0: #if player 1 is the only one left
              return [1,0,0]
-        elif count['1']==1 and count['2']==1:
+        elif count['1']==1 and count['2']==1: #if only two pieces left it must be a draw
             return [0,1,0]
-        elif count['1']==0 and count['2']>0:
+        elif count['1']==0 and count['2']>0: #if player 2 is the only one left
             return [0,0,1]
-        return [0,0,0]
+        return [0,0,0] #return default with no winners or losers or draw
     def getMoves(self,position):
         #get the possible moves from the current player
         #return grid positions
@@ -109,12 +121,13 @@ class GameBoard:
                         if (pos[0]+(constraint[0]*2)>=0  and pos[0]+(constraint[0]*2)<8
                             and pos[1]+(constraint[1]*2)>=0 and pos[1]+(constraint[1]*2)<8
                             and self.grid[pos[0]+constraint[0]*2][pos[1]+constraint[1]*2]==None):
-                            coords.append([pos[0]+constraint[0]*2,pos[1]+constraint[1]*2])
-                            if [pos[0]+constraint[0]*2,pos[1]+constraint[1]*2] not in searched:
-                                currentpos.append([pos[0]+constraint[0]*2,pos[1]+constraint[1]*2])
+                            #add potential take and search the take position to see if it can double take or more
+                            coords.append([pos[0]+constraint[0]*2,pos[1]+constraint[1]*2]) 
+                            if [pos[0]+constraint[0]*2,pos[1]+constraint[1]*2] not in searched: #don't get stuck in circles
+                                currentpos.append([pos[0]+constraint[0]*2,pos[1]+constraint[1]*2]) #check next position
                             arr=self.routes.get(str([pos[0]+constraint[0]*2,pos[1]+constraint[1]*2]),[])
                             arr.append({str(pos):[pos[0]+constraint[0],pos[1]+constraint[1]]})
-                            self.routes[str([pos[0]+constraint[0]*2,pos[1]+constraint[1]*2])]=arr
+                            self.routes[str([pos[0]+constraint[0]*2,pos[1]+constraint[1]*2])]=arr #trace route 
                                                                  
             virgin=True #set once gone through
 
@@ -132,7 +145,7 @@ class GameBoard:
             #print("taken",end,self.routes)
             key=str(end)
             things=[]
-            while things!=None:
+            while things!=None: #follow the route of taking players
                 things=self.routes.get(key,None)
                 if things!=None:
                     for thing in things: #loop through each piece and delete
@@ -140,12 +153,10 @@ class GameBoard:
                             #print("remove",thing[k])
                             self.grid[thing[k][0]][thing[k][1]]=None
                             key=k
-                        
-            
         #switch the positions of new and old
         pos=copy.deepcopy(self.grid[x][y])
         self.grid[x][y]=None #rewrite
-        self.grid[end[0]][end[1]]=copy.deepcopy(pos)
+        self.grid[end[0]][end[1]]=copy.deepcopy(pos) 
         if end[0]==0 and self.grid[end[0]][end[1]].getPlayer()==2: #made it to end
             self.grid[end[0]][end[1]].crown()
         if end[0]==7 and self.grid[end[0]][end[1]].getPlayer()==1: #made it to end
@@ -201,6 +212,8 @@ class main:
                         if self.board.grid[boardRow][boardColumn].getCrowned(): colour=(100,0,0)
                     pygame.draw.circle(self.screen,colour,(cx,cy),self.radius) #draw checker
     def getGrid(self,pos):
+        #get the index of the grid based off of coordinates the user has clicked
+        #@param pos containing the coordinates of the pixels pressed
         foundIndex=[-1,-1]
         for boardRow in range(8):
             for boardColumn in range(8):
@@ -210,8 +223,10 @@ class main:
                     foundIndex[0]=boardRow
                 if xCoordinate<pos[0] and pos[0]<xCoordinate+self.height:
                     foundIndex[1]=boardColumn
-        return foundIndex
+        return foundIndex #return the grid index
     def getSquare(self,poses):
+        #use the position to get the corner coordinates
+        #@param poses which contains the grid coordinates
         coords=[] #get coord of each position 
         for pos in poses: #get each position and check it works
             xCoordinate=((0+self.width) * pos[1] + 0)+self.xDistanceFromEdge
@@ -219,8 +234,10 @@ class main:
             coords.append([xCoordinate,yCoordinate])
         return coords
     def mainLoop(self):
+        #main game loop called upon
+        self.__init__() #reset game board
         self.display()
-        self.displayBoard()
+        self.displayBoard() #display the empty board
         pygame.display.flip()
         done=False
         currentPlayer=2
@@ -228,14 +245,14 @@ class main:
         scoresT=[]
         while not done: #loop through all the items
             for event in pygame.event.get(): #get each event
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT: #quit if quit button pressed
                     done = True
-                elif event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.MOUSEBUTTONDOWN: #if mouse is clicked
                     pos = pygame.mouse.get_pos()
                     grid=self.getGrid(pos) #evaluate the grid position
                     if -1 not in grid: #the selection is on the grid
                         #initiate game mechanics
-                        print(grid)
+                        #print(grid)
                         if (self.board.grid[grid[0]][grid[1]]!=None and self.board.grid[grid[0]][grid[1]].getPlayer()==currentPlayer
                             and (toggled==None or grid==toggled)):
                             self.board.grid[grid[0]][grid[1]].toggleSelected()
@@ -274,10 +291,24 @@ class main:
                         self.displayBoard() #display new board
                         textsurface = self.myfont.render("HAL: Just what do you think you're doing, Dave?", False, (255, 255, 255))
                         self.screen.blit(textsurface,(343,573))
+                #check whether or not there is a win
+                gamestate=self.board.getWinDrawLose()
+                if gamestate!=[0,0,0]: #check whether the game needs to end
+                    done=True
+                    pygame.display.flip()
+                    if gamestate==[1,0,0]: #player loses
+                        textsurface = self.myfont.render("HAL: You lose. I'm sorry, Frank, I think you missed it.", False, (255, 255, 255))
+                    elif gamestate==[0,0,1]: #player wins
+                        textsurface = self.myfont.render("HAL: You win. Thank you for a very enjoyable game.", False, (255, 255, 255))
+                    else: #draw
+                        textsurface = self.myfont.render("HAL: Its a draw!", False, (255, 255, 255))
+
+                    self.screen.blit(textsurface,(343,573))
                 pygame.display.flip()
-                    
+        time.sleep(5) #leave 5 seconds to
         self.exit()
     def exit(self):
+        textsurface = self.myfont.render("HAL: Goodbye", False, (255, 255, 255))
         print("Goodbye!")
         pygame.quit()
 
@@ -285,7 +316,6 @@ class main:
 #HAL quotes
 #I'm sorry, Frank, I think you missed it.
 #Thank you for a very enjoyable game.
-#I'm sorry, Frank, I think you missed it.
 #Just what do you think you're doing, Dave?
 
 game=main()
